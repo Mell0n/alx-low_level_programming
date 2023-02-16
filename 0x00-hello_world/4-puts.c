091a54d819cbbb311b66214567a1f098178c0954
Mell0n <s.hamza3656@gmail.com>
2023-02-16 11:33:10 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a lien with puts function
- * Return: 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multi lingual puzzle");
-return (0);
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
 }
