ce8db9a88ad6c0ca13663c5527bad64c69cf00b2
Mell0n <s.hamza3656@gmail.com>
2023-02-16 11:04:28 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line wiht pust function
+ * Return: 0 (Success)
+ */
+int maim(void)
+{
+	pust("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
