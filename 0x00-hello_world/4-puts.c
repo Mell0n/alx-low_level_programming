5d2fb8abdf7e1ad0880ffcb8dc5855414243cd0a
Mell0n <s.hamza3656@gmail.com>
2023-02-16 14:57:03 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - print line without printf
- *
+ * main - print line with puts
  * Return: Always 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multi lingual puzzle");
