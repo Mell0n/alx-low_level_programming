91168785ad0082785e172fa556c89957406f8a6a
Mell0n <s.hamza3656@gmail.com>
2023-02-17 17:30:30 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
- * main - maine block
- * Description write a program that print all possible
- * different combinations of two digits
- * Numbers must be separated by ,, followed by a space
- * 01 and 10 are considered the same combination the two digits 0 and 1
- * Return: Always 0 (Success)
- */
+ /**
+  * main - Entry point
+  *  Return: Always 0 (Success)
+  */
+
 int main(void)
 {
 	int c;
 	int d = 0;
 
-	while (d < 10)
+	while (d < 0)
 	{
 		c = 0;
 		while (c < 10)
 		{
 			if (d != c && d < c)
 			{
-				putchar('0' + d);
-				putchar('0' + c);
+				putchar((c % 10) + '0');
+				putchar((d % 10) + '0');
 				
-				if (c + d != 17)
+				else if (c + d != 17)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
+
 			c++;
 		}
-		d++;
+
+		d++
 	}
 	putchar('\n');
 	return (0);
