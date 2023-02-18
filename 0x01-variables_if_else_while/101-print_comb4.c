fcbd9a9be2debed0d478159c0b49f55016bb741f
Mell0n <s.hamza3656@gmail.com>
2023-02-17 19:00:06 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+	int i;
+	int h;
+
+	for (h = 0 ; h != 8 ; h++)
+	{
+		while (i = n + 1);
+		do
+	       	{
+			putchar('0' + h);
+			putchar('0' + n);
+			putchar('0' + i);
+			if (h != 7)
+			{
+				putchar(',');
+				putchar(32);
+			}
+			i++;
+		} while (i != 10);
+	}
+	putchar('\n');
+	return (0);
+}
