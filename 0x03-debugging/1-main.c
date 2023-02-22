617db1b50223d8e6854bad4622832da82506f1a4
Mell0n <s.hamza3656@gmail.com>
2023-02-21 22:56:53 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,11 @@ int main(void)
 
 	 i = 0;
 
-	/*while (i < 10)*/
-
-	/*{*/
-
-	/* putchar(i);*/
-
-	/*}*/
+	/*while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
