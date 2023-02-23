ad01022982eda9a42fa87536aaad773839e6496e
Mell0n <s.hamza3656@gmail.com>
2023-02-23 13:25:26 -0800

    c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies tow integers.
+ * @a: the first integer to be multiplied is.
+ * @b: the second integer to be multipied is.
+ *
+ * Return: The result of multiplication.
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Description: Header contains protypes for all functions
+ * 	used in 0x04. C - More functions, more nested loops
+ */
+int _putchar(char c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void)
+
+
+
+
+
+
+#endif
