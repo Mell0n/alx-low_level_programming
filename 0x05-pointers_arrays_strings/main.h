c9ca540b2ea4e501793bf4449d26e8754d3cbdeb
Mell0n <s.hamza3656@gmail.com>
2023-02-27 07:57:17 -0800

    i

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,10 @@
 
 int _putchar(char c);
 
+int _atoi(char *s);
+
+char *_strcpy(char *dest, char *src);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -26,6 +30,5 @@ void puts_half(char *str);
 
 void print_array(int *a, int n);
 
-char *_strcpy(char *dest, char *src);
 
 #endif
